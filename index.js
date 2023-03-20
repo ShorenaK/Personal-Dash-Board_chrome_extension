2a786dfd7d74503bafa7515fe086b131ab602016
ShorenaK <shorenaanzhilov@gmail.com>
2023-03-20 15:20:00 -0400

    fetching dogecoin prices

--- index.js
+++ index.js
@@ -28,7 +28,12 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     // throw Error -- manualy throws an error 
       document.getElementById("crypto-top").innerHTML = 
       `<img src="${data.image.small}"/> 
-        <p> ${data.name}</p>
+        <span> ${data.name}</span>`
+        document.getElementById("crypto").innerHTML +=
+        `<p>🎯: ${data.market_data.current_price.usd}</p>
+         <p>👆: ${data.market_data.high_24h.usd}</p>
+         <p>👇: ${data.market_data.low_24h.usd}</p>
+        </div>
       `
     console.log(data)
 })
